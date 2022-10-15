3ca6bdc8578cd32db1dc096166d368b1f08f606e
lls <lls@DESKTOP-Q4U2V1A>
2022-10-15 17:08:10 +0200

    Subiendo la segunda clase.

---
+++ Probando2223/src/prueba01/Clase02.java
@@ -0,0 +1,11 @@
+package prueba01;
+
+public class Clase02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esta es la segunda clase.");
+	}
+
+}
